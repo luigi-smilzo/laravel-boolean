05c527d8bb4c7a1e7e2597e6eef42efbc3879fcf
Luigi <luigismilzo@live.com>
2020-06-11 15:56:52 +0200

    Route setup and statica pages controller setup

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Static pages
+Route::get('/', 'StaticPageController@index')->name('static-page.home');
+Route::get('/privacy', 'StaticPageController@privacy')->name('static-page.privacy');
+Route::get('/faq', 'StaticPageController@faq')->name('static-page.faq');
+
+// Students
+Route::get('/students', 'StudentsController@index')->name('students.index');
+Route::get('/students/{id}', 'StudentsController@show')->name('students.show');
