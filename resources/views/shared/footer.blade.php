566d374436c7a384bb50d8765be5dbc5644d6f97
Luigi <luigismilzo@live.com>
2020-06-11 16:54:21 +0200

    Privacy policy structure

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,6 +1,11 @@
 <footer class="MainFooter">
     <div class="MainFooter-left">
         <img src="https://www.boolean.careers/images/common/logo-white.png" alt="">
+        <div class="Addresses">
+            <span>Sede Operativa</span>
+            <span>Via Carducci 12 - 20123 Milano</span>
+            <span>Tel: 02-40031288</span>
+        </div>
     </div>
     <div class="MainFooter-right">
         <ul>
--- resources/views/static-pages/index.blade.php
+++ resources/views/static-pages/index.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Hello</h1>
+    <div class="Home">
+        <div class="Jumbotron">
+            <div class="Jumbotron-text">
+                <h1>diventa <strong>sviluppatore web</strong></h1>
+                <p>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</p>
+                <ul>
+                    <li><strong>6 mesi</strong> di corso intensivo online in diretta</li>
+                    <li><strong>Nessuna competenza</strong> di rogrammazione richiesta</li>
+                    <li>Siamo certi del tuo successo, altrimenti <strong>ti rimborsiamo</strong></li>
+                </ul>
+            </div>
+            <div class="Jumbotron-picture">
+                <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="">
+            </div>
+        </div>
+    </div>
 @endsection
