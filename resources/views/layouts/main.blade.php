bf661b33dddf3a5d8b43e3992d4bba165a7f20ae
Luigi <luigismilzo@live.com>
2020-06-12 17:07:25 +0200

    Style setup: font, vars, typography

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Laravel Boolean</title>
 </head>
 <body>
