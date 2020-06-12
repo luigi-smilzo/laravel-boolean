512d263737ed02728b1996a5dcbdeec641bf5403
Luigi <luigismilzo@live.com>
2020-06-12 18:59:25 +0200

    Added navbar highlight on active page

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,12 +1,12 @@
-<header class="MainHeader">
+<header class="MainHeader u-flex u-justifyBtw">
     <div class="MainHeader-logo">
         <img src="https://www.boolean.careers/images/common/logo.png" alt="boolean">
     </div>
-    <nav class="MainHeader-nav">
-        <ul>
-            <li><a href="{{ route('static-page.home') }}">home</a></li>
+    <nav class="MainHeader-nav u-flex u-alignCtr">
+        <ul class="u-flex">
+            <li class="{{ Request::is('/') ? 'isActive' : '' }}"><a href="{{ route('static-page.home') }}">home</a></li>
             <li><a href="#">corso</a></li>
-            <li><a href="{{ route('students.index') }}">dopo il corso</a></li>
+            <li class="{{ Request::is('students') ? 'isActive' : '' }}"><a href="{{ route('static-page.home') }}"><a href="{{ route('students.index') }}">dopo il corso</a></li>
             <li><a href="#">lezione gratuita</a></li>
             <li><a href="#">assumi i nostri studenti</a></li>
             <li><a href="#">candidati ora</a></li>
